9bae8196629b9da016d6be963b8828abd1276d5e
FVangelista <119606051+FVangelista@users.noreply.github.com>
2022-12-20 14:47:57 +0100

    esercizio 20-12-2022

---
+++ 20-12-2022/app.js
@@ -0,0 +1,30 @@
+let cars = [];
+
+// for (let i = 0; i < 10; i++) {
+//   cars.push(prompt('Insert your favorite cars: '));
+//   if (i === 9) {
+//     for (let j = 0; j < cars.length; j++) {
+//       console.log(cars[j]);
+//     }
+//   }
+// }
+
+// advanced
+
+// for (let i = 10; i > 0; i--) {
+//   cars.push(prompt('Insert your favorite cars: '));
+//   if (i === 1) {
+//     for (let j in cars) {
+//       console.log(j);
+//     }
+//   }
+// }
+
+// for (let i = 10; i > 0; i--) {
+//   cars.push(prompt('Insert your favorite cars: '));
+//   if (i === 1) {
+//     for (let j of cars) {
+//       console.log(j);
+//     }
+//   }
+// }
