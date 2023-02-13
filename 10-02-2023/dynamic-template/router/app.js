00da386bf209e7c0c307c9aa2cace6b3e03baba4
Filippo Bortoletti <filippobortoletti@MBP-di-Filippo.home>
2023-02-13 18:26:53 +0100

    login 13-02-2023

--- 10-02-2023/dynamic-template/router/app.js
+++ 10-02-2023/dynamic-template/router/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = require('./router');
+const session = require('cookie-session');
+const bodyParser = require('body-parser');
 const hbs = require('hbs');
 
 const app = express();
 const PORT = 8000;
 
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'my session' }));
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +23,10 @@ app.get('/about', router);
 app.get('/contatti', router);
 app.get('/formazione', router);
 app.get('/web-design', router);
+app.get('/signup', router);
+app.post('/signup', router);
+app.get('/private', router);
+app.get('/warning', router);
 app.get('*', router);
 
 app.listen(PORT, () => {
--- 10-02-2023/dynamic-template/router/router.js
+++ 10-02-2023/dynamic-template/router/router.js
@@ -32,6 +32,57 @@ router.get('/web-design', (req, res) => {
   });
 });
 
+router.get('/signup', (req, res) => {
+  res.render('signup', {
+    title: 'Signup',
+  });
+});
+
+router.post('/signup', (req, res) => {
+  // console.log(`${req.body.username} / ${req.body.password}`);
+
+  if (!req.body.username || !req.body.password) {
+    res.status(400);
+    res.render('signup', { message: 'Insert username and password' });
+  } else {
+    let name = 'filippo';
+    let password = 12345;
+    let userAuth = { name: name, password: password };
+    if (name == req.body.username && password == req.body.password) {
+      req.session.user = userAuth;
+      res.redirect('/private');
+    } else {
+      res.redirect('/warning');
+    }
+  }
+
+  // res.render('signup');
+});
+
+// protection control - private page
+
+function checkPage(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/signup');
+  }
+}
+
+router.get('/private', checkPage, (req, res) => {
+  res.render('private');
+});
+
+router.get('/warning', (req, res) => {
+  res.render('warning');
+});
+
+router.get('/logout', (req, res) => {
+  console.log('logout executed..');
+  req.session = null;
+  res.redirect('/signup');
+});
+
 router.get('*', (req, res) => {
   res.send('404 | page not found');
 });
